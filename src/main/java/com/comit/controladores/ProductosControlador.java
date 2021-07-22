033b03e9d105e37922be70146760cbfe27fa5b23
ramip <ramip@LAPTOP-8147CPEQ>
2021-07-22 15:18:52 -0300

    1

---
+++ src/main/java/com/comit/controladores/ProductosControlador.java
@@ -0,0 +1,17 @@
+package com.comit.controladores;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/productos")
+public class ProductosControlador {
+
+	@RequestMapping (value = "/comprar", method = RequestMethod.GET)
+	
+	public String comprarProducto() {
+	return "comprarProducto";
+
+}
+}
